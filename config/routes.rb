8dc65964adc7bb11a10b0477578c170ace5aa273
Martin Delbrück <info@martindelbrueck.de>
2012-03-12 15:16:04 +0100

    * remove index.html and set root to survey controller

--- config/routes.rb
+++ config/routes.rb
@@ -57,7 +57,7 @@ OSurvey::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'surveys#index'
 
   # See how all your routes lay out with "rake routes"
 
