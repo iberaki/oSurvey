d99c1d2a8c11ba3b608f21afefbb27e18fb52513
Martin Delbrück <info@martindelbrueck.de>
2012-03-12 15:13:40 +0100

    * some small controller function

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -9,13 +9,36 @@ class SurveysController < ApplicationController
       @survey = Survey.find(params[:id])
     end
   end
+  
+  def edit
+    @survey = Survey.find(params[:id])
+  end
 
   def create
+    @survey = Survey.new(params[:survey])
+
+    if @survey.save
+      redirect_to(surveys_path, :notice => t('notice.survey.create_succesfull'))
+    else
+      render :action => 'new'
+    end
+  end
+
+  def update
+    @survey = Survey.find(params[:id])
+    @survey.attributes = params[:survey]
+
+    if @survey.save
+      redirect_to(surveys_path, :notice => t('notice.survey.update_succesfull'))
+    else
+      render :action => 'edit'
+    end
   end
 
   def destroy
   end
 
   def new
+    @survey = Survey.new()
   end
 end
