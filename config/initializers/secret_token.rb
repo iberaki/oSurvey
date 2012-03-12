55ba1e16ab6ddf18a5cebcfd1a3aa06704e8a9fb
Martin Delbrück <info@martindelbrueck.de>
2012-03-12 11:03:47 +0100

    new Railsproject

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OSurvey::Application.config.secret_token = 'dd9602b0376558fca2ed4b587bfc768b21d59303a09000772fd2811c45edf6168c8d7b58178bd53dd9ed7234fa6859304d96551bec488b52f9fe7953bb032bcf'
