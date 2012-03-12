716b1197c0a64ec8555577096279c9549ffa02d2
Martin Delbrück <info@martindelbrueck.de>
2012-03-12 14:12:32 +0100

    * first controller action

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,8 +1,13 @@
 class SurveysController < ApplicationController
+  
   def index
+    @surveys = Survey.all
   end
 
   def show
+    if(curren_user == Survey.find(params[:id]).user_id)
+      @survey = Survey.find(params[:id])
+    end
   end
 
   def create
